22c8465fcac632e64a1bc5b214acb0a62ba24c83
vaweto <vagelisbisbikis@gmail.com>
2019-05-19 21:01:47 +0300

    invitation user with test and formrequest

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -14,7 +14,47 @@ class InvitationsTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    function a_project_can_invite_a_user()
+    function non_owners_may_not_invite_users()
+    {
+        $this->actingAs(factory(User::class)->create())
+            ->post(ProjectFactory::create()->path() . '/invitations')
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    function a_projects_owner_can_invite_a_users()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = ProjectFactory::create();
+
+        $userToInvite = factory(User::class)->create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+            'email' => $userToInvite->email
+            ])
+            ->assertRedirect($project->path());
+
+        $this->assertTrue($project->members->contains($userToInvite));
+    }
+
+    /** @test */
+    function the_invited_email_address_mustbe_valid_biboard_account()
+    {
+        $project = ProjectFactory::create();
+
+        $this->actingAs($project->owner)
+            ->post($project->path() . '/invitations', [
+            'email' => 'not@user.com'])
+            ->assertSessionHasErrors(
+                ['email' => 'must have a app account']
+            );
+
+    }
+
+    /** @test */
+    function invited_users_may_update_projects_details()
     {
         $project = ProjectFactory::create();
 
